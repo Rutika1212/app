b71784701eff588e60e840a89f081be39a3ec01d
Rutika1212 <85065158+Rutika1212@users.noreply.github.com>
2024-05-01 13:17:10 +0530

    Update Recommendation.py

--- Recommendation.py
+++ Recommendation.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from xgboost import DMatrix
 
 # Snowflake Connection Details
-SNOWFLAKE_ACCOUNT = 'WOTFAMO.BS88857.ap-southeast-1.AWS'
+SNOWFLAKE_ACCOUNT = 'WOTFAMO.BS88857'
 SNOWFLAKE_USER = 'Rutika'
 SNOWFLAKE_PASSWORD = 'Rutika@12'
 SNOWFLAKE_WAREHOUSE = 'COMPUTE_WH'
